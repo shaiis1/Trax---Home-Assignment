9ecd21791fe7d439fa2e2ae7cc8f86ba95e19738
shayi <shayi@gulliver.co.il>
2020-10-28 11:09:32 +0200

    Adding test file using chai module

---
+++ test.js
@@ -0,0 +1,48 @@
+require('../app.js');
+var expect = require('chai').expect;
+var request = require('request');
+
+let contactsArray = [{ name: 'Shai', phone: '0503793797' },
+{  name: 'Moran', phone: '0542248433'  },
+ {  name: 'Sarai', phone: '0503793796'  }, 
+ {  name: 'Guy', phone: '0505568794'  },
+{ name: 'Benny', phone: '0507821635' },
+{ name: 'Yossi', phone: '0507821689' }]
+
+describe('Basic GET POST tests', function () {
+    it('should GET the greeting - Hi Trax! This is Shai Israeli', function() {
+      request.get('http://localhost:4000/whos-there', function (error, response) {
+        expect(response.statusCode).to.equal(200);
+        expect(response.body).to.equal('Hi Trax! This is Shai Israeli') 
+    });
+    })
+  
+    it('Should GET all exsiting contacts', function () {
+      request.get('http://localhost:4000/getAllContacts', function(error, response) {
+        expect(response.statusCode).to.equal(200);
+        expect(response.body).to.equal(JSON.stringify(contacts));
+      });
+    });
+
+    it('Should GET error message - The requested contact name was not found!', function(){
+        request.get('http://localhost:4000/getContactByName/avi', function(error, response) {
+        expect(response.statusCode).to.equal(404);
+        expect(response.body).to.equal('The requested contact name was not found!');
+        });
+    });
+
+    it('Should GET the requested contact by the contact name', function(){
+        request.get('http://localhost:4000/getContactByName/shai'), function(error, response) {
+            expect(response.statusCode).to.equal(200);
+            expect(response.body).to.equal({ name: 'Shai', phone: '0503793797' })
+        }
+    })
+
+    it('Should POST a new contact into the contacts array', function(){
+        request.post('http://localhost:4000/addNewContact', 
+        {json: {"name":"checkName","phone":"0523574492"}}, function(error, response){
+            expect(response.statusCode).to.equal(200);
+            expect(response.body).to.equal('Contact was added successfully!')
+        })
+    })
+  });
